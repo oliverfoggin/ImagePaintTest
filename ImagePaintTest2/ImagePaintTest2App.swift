e4bb15dace90b82461f2307dfa4db925efc5ade5
Oliver Foggin <oliverfoggin@me.com>
2023-09-08 20:21:24 +0100

    Update ImagePaintTest2App.swift

--- ImagePaintTest2/ImagePaintTest2App.swift
+++ ImagePaintTest2/ImagePaintTest2App.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct ImagePaintTest2App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+	var body: some Scene {
+		WindowGroup {
+			VStack {
+				ContentView()
+					.background(Color.white)
+	        }
+	        .frame(maxWidth: .greatestFiniteMagnitude, maxHeight: .greatestFiniteMagnitude)
+	        .background()
+	        .backgroundStyle(ImagePaint(image: Image("Image")))
         }
     }
 }
